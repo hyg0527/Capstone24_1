2e33851826a53059689cff0bba8c45f96bd70a9c
hyg0527 <yoongoo0807@gmail.com>
2024-01-24 20:37:22 +0900

    lottie animation added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.credential.cubrism"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.credential.cubrism"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -41,6 +41,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("com.airbnb.android:lottie:6.3.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
