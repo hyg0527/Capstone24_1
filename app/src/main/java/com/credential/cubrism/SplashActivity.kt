5bab0cb285229fde5af2f91538a0ed5022290e28
hyg0527 <yoongoo0807@gmail.com>
2024-01-25 16:47:03 +0900

    register/login edit - 1647

--- app/src/main/java/com/credential/cubrism/SplashActivity.kt
+++ app/src/main/java/com/credential/cubrism/SplashActivity.kt
@@ -24,6 +24,6 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 5000)
+        }, 6000)
     }
 }
