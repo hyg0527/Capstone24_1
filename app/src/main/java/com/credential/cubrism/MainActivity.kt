776b05484dc821279ad5ee8f58610dce88e4cfdc
hyg0527 <yoongoo0807@gmail.com>
2024-01-25 12:23:38 +0900

    main page add

--- app/src/main/java/com/credential/cubrism/MainActivity.kt
+++ app/src/main/java/com/credential/cubrism/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.credential.cubrism
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/credential/cubrism/RegisterActivity.kt
+++ app/src/main/java/com/credential/cubrism/RegisterActivity.kt
@@ -1,11 +1,17 @@
 package com.credential.cubrism
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val backBtn = findViewById<ImageButton>(R.id.imageButton)
+        backBtn.setOnClickListener {
+            finish()
+        }
     }
 }
