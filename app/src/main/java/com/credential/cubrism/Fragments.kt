34cc61279fc012b0cf039f6182ff260112ecae38
hyg0527 <yoongoo0807@gmail.com>
2024-02-12 21:04:00 +0900

    Fragments add
    알림화면 추가 - 종 클릭
    
    달력 추가 - 날짜 누르면 현재 날짜 출력
    
    FloatingActionButton 디자인 변경
    
    자격증 상세화면 추가
    
    프로필 변경 - 카메라 삭제, 기본 프로필로 변경 추가
    
    Fragments.kt 파일 분할 (너무 길어서 나눴음)

--- app/src/main/java/com/credential/cubrism/Fragments.kt
+++ app/src/main/java/com/credential/cubrism/Fragments.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,12 +16,33 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import de.hdodenhof.circleimageview.CircleImageView
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class StudyFragment : Fragment(R.layout.fragment_study) {
 
 }
-class CalFragment : Fragment(R.layout.fragment_cal) {
 
+class CalFragment : Fragment(R.layout.fragment_cal) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val calendarView = view.findViewById<CalendarView>(R.id.calendarView)
+        val currentDate = view.findViewById<TextView>(R.id.txtCurrentDate)
+
+        // 날짜 누르면 날짜를 textview에 출력
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            val selectedDate = "${year}년 ${month + 1}월 ${dayOfMonth}일"
+            currentDate.text = selectedDate
+        }
+
+        // 처음 생성시 오늘 날짜 기본 출력
+        val today = Calendar.getInstance().time
+        val todayDateFormat = SimpleDateFormat("yyyy년 M월 dd일", Locale.getDefault())
+        val todayString = todayDateFormat.format(today)
+
+        currentDate.text = todayString
+    }
 }
 
 class MyPageFragment : Fragment(R.layout.fragment_mypage) {
