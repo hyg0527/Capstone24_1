c600a96b27e008db6eaa82bbaf3cfadcbb86c6f5
caadiq <caadiq@gmail.com>
2024-04-03 19:33:21 +0900

    빈번한 오류 발생으로 암호화 삭제

--- app/src/main/java/com/credential/cubrism/model/data/DataStoreModule.kt
+++ app/src/main/java/com/credential/cubrism/model/data/DataStoreModule.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
-import com.credential.cubrism.security.KeystoreEncryptor
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
@@ -16,31 +15,29 @@ class DataStoreModule(private val context: Context) {
 
     // Access Token 저장
     suspend fun saveAccessToken(accessToken: String) {
-        val encryptedToken = KeystoreEncryptor.encrypt(accessToken)
         context.dataStore.edit { preferences ->
-            preferences[accessTokenKey] = encryptedToken
+            preferences[accessTokenKey] = accessToken
         }
     }
 
     // Access Token 불러오기
     fun getAccessToken(): Flow<String?> {
         return context.dataStore.data.map { prefs ->
-            prefs[accessTokenKey]?.let { KeystoreEncryptor.decrypt(it)}
+            prefs[accessTokenKey]
         }
     }
 
     // Refresh Token 저장
     suspend fun saveRefreshToken(refreshToken: String) {
-        val encryptedToken = KeystoreEncryptor.encrypt(refreshToken)
         context.dataStore.edit { preferences ->
-            preferences[refreshTokenKey] = encryptedToken
+            preferences[refreshTokenKey] = refreshToken
         }
     }
 
     // Refresh Token 불러오기
     fun getRefreshToken(): Flow<String?> {
         return context.dataStore.data.map { prefs ->
-            prefs[refreshTokenKey]?.let { KeystoreEncryptor.decrypt(it)}
+            prefs[refreshTokenKey]
         }
     }
 }
