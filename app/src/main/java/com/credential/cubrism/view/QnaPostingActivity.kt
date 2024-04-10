41007192625f7582fcc67e8a35b0664cf7414d4a
caadiq <caadiq@gmail.com>
2024-04-10 13:31:04 +0900

    Q&A 게시판 카테고리 추가

--- app/src/main/java/com/credential/cubrism/view/QnaPostingActivity.kt
+++ app/src/main/java/com/credential/cubrism/view/QnaPostingActivity.kt
@@ -1,20 +1,33 @@
 package com.credential.cubrism.view
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.viewModels
 import com.credential.cubrism.databinding.ActivityQnaPostingBinding
+import com.credential.cubrism.databinding.DialogCategoryBinding
 import com.credential.cubrism.model.dto.PostAddDto
 import com.credential.cubrism.model.repository.PostRepository
+import com.credential.cubrism.model.repository.QualificationRepository
 import com.credential.cubrism.model.utils.ResultUtil
+import com.credential.cubrism.view.adapter.QualificationAdapter
+import com.credential.cubrism.view.utils.ItemDecoratorDivider
 import com.credential.cubrism.viewmodel.PostViewModel
+import com.credential.cubrism.viewmodel.QualificationViewModel
 import com.credential.cubrism.viewmodel.ViewModelFactory
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class QnaPostingActivity : AppCompatActivity() {
     private val binding by lazy { ActivityQnaPostingBinding.inflate(layoutInflater) }
 
     private val postViewModel: PostViewModel by viewModels { ViewModelFactory(PostRepository()) }
+    private val qualfiicationViewModel: QualificationViewModel by viewModels { ViewModelFactory(QualificationRepository()) }
+
+    private val qualificationAdapter = QualificationAdapter()
+    private lateinit var bottomSheetDialog: BottomSheetDialog
 
     private val boardId = 1
 
@@ -23,6 +36,7 @@ class QnaPostingActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupToolbar()
+        setupBottomSheetDialog()
         setupView()
         observeViewModel()
     }
@@ -34,11 +48,35 @@ class QnaPostingActivity : AppCompatActivity() {
         binding.toolbar.setNavigationOnClickListener { finish() }
     }
 
+    private fun setupBottomSheetDialog() {
+        val bottomSheetBinding = DialogCategoryBinding.inflate(layoutInflater)
+        bottomSheetDialog = BottomSheetDialog(this)
+        bottomSheetDialog.setContentView(bottomSheetBinding.root)
+
+        bottomSheetBinding.recyclerView.apply {
+            adapter = qualificationAdapter
+            itemAnimator = null
+            addItemDecoration(ItemDecoratorDivider(0, 0, 0, 0, 2, 0, Color.GRAY))
+            setHasFixedSize(true)
+        }
+
+        bottomSheetBinding.editSearch.addTextChangedListener(onTextChanged = { text, _, _, _ ->
+            qualificationAdapter.filter.filter(text)
+        })
+
+        qualificationAdapter.setOnItemClickListener { item, _ ->
+            binding.txtCategory.text = item.name
+            bottomSheetDialog.dismiss()
+        }
+    }
+
     private fun setupView() {
+        qualfiicationViewModel.getQualificationList()
+
         binding.btnAdd.setOnClickListener {
             val title = binding.editTitle.text.toString()
             val content = binding.editContent.text.toString()
-            val category = "정보처리기사" // 임시
+            val category = binding.txtCategory.text.toString()
             val images = emptyList<String?>() // 임시
 
             if (title.isEmpty()) {
@@ -54,12 +92,27 @@ class QnaPostingActivity : AppCompatActivity() {
             val postAddDto = PostAddDto(title, content, boardId, category, images)
             postViewModel.addPost(postAddDto)
         }
+
+        binding.imgDropDown.setOnClickListener {
+            bottomSheetDialog.show()
+        }
     }
 
     private fun observeViewModel() {
         postViewModel.addPost.observe(this) { result ->
             when (result) {
                 is ResultUtil.Success -> { setResult(RESULT_OK).also { finish() } }
+                is ResultUtil.Error -> { Toast.makeText(this, result.error, Toast.LENGTH_SHORT).show() }
+                is ResultUtil.NetworkError -> { Toast.makeText(this, "네트워크 오류가 발생했습니다.", Toast.LENGTH_SHORT).show() }
+            }
+        }
+
+        qualfiicationViewModel.qualificationList.observe(this) { result ->
+            when (result) {
+                is ResultUtil.Success -> {
+                    binding.txtCategory.text = result.data.random().name
+                    qualificationAdapter.setItemList(result.data)
+                }
                 is ResultUtil.Error -> { Toast.makeText(this, result.error, Toast.LENGTH_SHORT).show() }
                 is ResultUtil.NetworkError -> { Toast.makeText(this, "네트워크 오류가 발생했습니다.", Toast.LENGTH_SHORT).show() }
             }
