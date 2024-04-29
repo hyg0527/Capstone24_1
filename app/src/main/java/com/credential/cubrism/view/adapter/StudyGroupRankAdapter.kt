ae929b47cf613b1d1736e5ccfd21e6faf7a12915
hyg0527 <yoongoo0807@gmail.com>
2024-04-29 16:15:17 +0900

    drawer, rank 레이아웃 수정

--- app/src/main/java/com/credential/cubrism/view/adapter/StudyGroupRankAdapter.kt
+++ app/src/main/java/com/credential/cubrism/view/adapter/StudyGroupRankAdapter.kt
@@ -2,27 +2,32 @@ package com.credential.cubrism.view.adapter
 
 import android.animation.ObjectAnimator
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.credential.cubrism.R
+import com.credential.cubrism.databinding.ItemStudygroupRankBinding
 
 data class Rank(val userName: String? = null, val percentage: Int? = null)
 
 class StudyGroupRankAdapter(private val items: ArrayList<Rank>) : RecyclerView.Adapter<StudyGroupRankAdapter.RankViewHolder>() {
-    inner class RankViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-        val userName = v.findViewById<TextView>(R.id.txtUserNameRank)
-        val percentage = v.findViewById<ProgressBar>(R.id.progressBar)
-        val perString = v.findViewById<TextView>(R.id.txtPercentageRank)
+    class RankViewHolder(private val binding: ItemStudygroupRankBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Rank) {
+            val perStringTxt = "${item.percentage}%"
+            val finalProgress = (item.percentage ?: 0).toInt()
+            // 모션 생성
+            val animation = ObjectAnimator.ofInt(binding.progressBar, "progress", 0, finalProgress)
+            animation.duration = 800
+            animation.start()
+
+            binding.txtUserNameRank.text = item.userName
+            binding.progressBar.progress = finalProgress
+            binding.txtPercentageRank.text = perStringTxt
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RankViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_studygroup_rank, parent, false)
-
-        return RankViewHolder(view)
+        val binding = ItemStudygroupRankBinding.inflate(inflater, parent, false)
+        return RankViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -30,20 +35,10 @@ class StudyGroupRankAdapter(private val items: ArrayList<Rank>) : RecyclerView.A
     }
 
     override fun onBindViewHolder(holder: RankViewHolder, position: Int) {
-        val perStringTxt = items[position].percentage.toString() + "%"
-
-        val finalProgress = (items[position].percentage ?: 0).toInt()
-        val animation = ObjectAnimator.ofInt(holder.percentage, "progress", 0, finalProgress) // 애니메이션 생성
-        animation.duration = 1000 // 애니메이션 지속 시간 설정 (밀리초 단위)
-        animation.start()        // 애니메이션 시작
-
-        holder.userName.text = items[position].userName
-        holder.percentage.progress = finalProgress
-        holder.perString.text = perStringTxt
-
+        holder.bind(items[position])
     }
 
-    fun reloadItems() { // 새로고침 함수
+    fun reloadItems() {
         notifyDataSetChanged()
     }
 }
