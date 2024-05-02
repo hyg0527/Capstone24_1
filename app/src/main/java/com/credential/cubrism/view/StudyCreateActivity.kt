4967b33f012e89b91e478b465e54ef31d06d5758
caadiq <caadiq@gmail.com>
2024-05-02 14:14:27 +0900

    주석 제거

--- app/src/main/java/com/credential/cubrism/view/StudyCreateActivity.kt
+++ app/src/main/java/com/credential/cubrism/view/StudyCreateActivity.kt
@@ -114,37 +114,4 @@ class StudyCreateActivity : AppCompatActivity(), TagDeleteClickListener {
                 setText(getText().toString().substring(1))
         }
     }
-
-//    private fun submitButtonPressed() { // 가입 하기 버튼 눌렀을 때 처리 하는 함수
-//        val numS = binding.numS.text.toString()
-//
-//        val numSInt = if (numS.isNotEmpty()) {
-//            numS.toInt()
-//        } else 0 // 기본 값은 0으로 설정
-//
-//        when {
-//            (binding.editGroupName.text.isEmpty()) ->
-//                Toast.makeText(this, "그룹 명을 입력하세요.", Toast.LENGTH_SHORT).show()
-//
-//            (numS.isEmpty()) -> Toast.makeText(this, "스터디 그룹 인원 수를 입력하세요.", Toast.LENGTH_SHORT).show()
-//            (numSInt <= 1) -> Toast.makeText(this, "스터디 그룹 인원 수를 2명 이상 입력하세요.", Toast.LENGTH_SHORT).show()
-//
-//            (binding.editInfo.text.isEmpty()) ->
-//                Toast.makeText(this, "스터디 그룹 설명을 입력하세요.", Toast.LENGTH_SHORT).show()
-//
-//            else -> {
-//                hideKeyboard()
-//                Toast.makeText(this, "스터디를 등록하였습니다.", Toast.LENGTH_SHORT).show()
-//                finish()
-//            }
-//        }
-//    }
-//
-//    // 뷰에 포커스를 주고 키보드를 숨기는 함수
-//    private fun hideKeyboard() {
-//        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//        currentFocus?.let { view ->
-//            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
-//        }
-//    }
 }
