9e5f73c5e3c4621499f2cae30155900645e4ff73
caadiq <caadiq@gmail.com>
2024-05-09 16:46:39 +0900

    Splash 화면 추가

--- app/src/main/java/com/credential/cubrism/view/SplashActivity.kt
+++ app/src/main/java/com/credential/cubrism/view/SplashActivity.kt
@@ -1,34 +1,62 @@
 package com.credential.cubrism.view
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.credential.cubrism.MyApplication
 import com.credential.cubrism.databinding.ActivitySplashBinding
+import com.credential.cubrism.model.repository.AuthRepository
+import com.credential.cubrism.viewmodel.AuthViewModel
+import com.credential.cubrism.viewmodel.ViewModelFactory
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     private val binding by lazy { ActivitySplashBinding.inflate(layoutInflater) }
+
+    private val myApplication = MyApplication.getInstance()
+
+    private val authViewModel: AuthViewModel by viewModels { ViewModelFactory(AuthRepository()) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.lotti.speed = 1.5f
-        splashScreenLoad()
+        observeViewModel()
+
+        authViewModel.getUserInfo()
     }
 
-    private fun splashScreenLoad() {
-        // 5초 딜레이 후에 MainActivity 호출
-        val handlerThread = HandlerThread("HandlerThread")
-        handlerThread.start()
-        val handler = Handler(handlerThread.looper)
+    private fun observeViewModel() {
+        authViewModel.apply {
+            getUserInfo.observe(this@SplashActivity) { user ->
+                // 서버에서 유저 정보를 성공적으로 받아오면 유저 정보를 저장
+                myApplication.getUserData().apply {
+                    setLoginStatus(true)
+                    setUserData(user.email, user.nickname, user.profileImage)
+                }
 
-        handler.postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+                startMainActivity()
+            }
+
+            // 유저 정보를 받아오지 못하면 메인 액티비티로 이동
+            errorMessage.observe(this@SplashActivity) { event ->
+                event.getContentIfNotHandled()?.let {
+                    startMainActivity()
+                }
+            }
+        }
+    }
+
+    private fun startMainActivity() {
+        lifecycleScope.launch {
+            delay(2000) // 2초 딜레이 (임시)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
-        }, 5000)
+        }
     }
 }
